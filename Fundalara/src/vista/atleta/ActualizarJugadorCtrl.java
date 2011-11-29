563ba56f4924e8a7f8508d1b59cf66f0db1bfc0d
Robert Arrieche <robertArrieche@172.19.13.182>
2011-11-29 08:13:30 -0430

    Actualizacion defensa

--- Fundalara/src/vista/atleta/ActualizarJugadorCtrl.java
+++ Fundalara/src/vista/atleta/ActualizarJugadorCtrl.java
@@ -1,18 +1,36 @@
 package vista.atleta;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import org.zkoss.util.media.Media;
+import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.util.GenericForwardComposer;
-import org.zkoss.zul.Image;
+import org.zkoss.zul.*;
+
 import vista.comun.FileLoader;
 
 
 public class ActualizarJugadorCtrl extends GenericForwardComposer {
 
-	private static final long serialVersionUID = -5912226982837429181L;
-	private Image imgJugador;
+	private Image img;
+	
+	
 
+	
 	public void onClick$btnLoad() {
-		new FileLoader().cargarImagen(imgJugador);
-
+		new FileLoader().cargarImagen(img);
+		
+		
+	
 	}
+
+	
+
+	
 
 }
