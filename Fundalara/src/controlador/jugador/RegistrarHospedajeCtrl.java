e63f91cddd59c6c4c37a064aaf989bfa9354216e
GermanLaguna <GermanLaguna@190.199.143.239>
2011-12-17 10:17:45 -0400

    Actualizacion Hospedaje vista - controlador (Equipo 2)

--- Fundalara/src/controlador/jugador/RegistrarHospedajeCtrl.java
+++ Fundalara/src/controlador/jugador/RegistrarHospedajeCtrl.java
@@ -1,81 +1,187 @@
 package controlador.jugador;
 
-import java.util.Date;
+import java.util.List;
+
+import modelo.Curso;
+import modelo.Competencia;
+import modelo.Hospedaje;
+import modelo.HospedajeJugador;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.event.ForwardEvent;
 import org.zkoss.zk.ui.util.GenericForwardComposer;
+import org.zkoss.zkplus.databind.AnnotateDataBinder;
 import org.zkoss.zul.Button;
 import org.zkoss.zul.Combobox;
 import org.zkoss.zul.Datebox;
-import org.zkoss.zul.Grid;
 import org.zkoss.zul.Image;
 import org.zkoss.zul.Intbox;
-import org.zkoss.zul.Messagebox;
+import org.zkoss.zul.Listbox;
 import org.zkoss.zul.Textbox;
 import org.zkoss.zul.Window;
-import org.zkoss.zul.api.Tab;
+
+import servicio.jugador.ServicioCurso;
 
 import comun.FileLoader;
+import comun.Rutas;
 import comun.Util;
 
-
 /**
- * Clase controladora de los eventos de la vista de igual nombre.
+ * Clase controladora de los eventos de la vista de igual nombre y manejo de los
+ * servicios de datos para el registro de hospedaje
  * 
- * @author Edgar L 
+ * @author Edgar L
+ * @author Erika O
+ * @version 0.2 16/12/2011
  * 
- * @version 0.1 10/12/2011
- *
  * */
 
 public class RegistrarHospedajeCtrl extends GenericForwardComposer {
-	private Window registrarHospedaje;
-	
+
+	private Window winRegistrarHospedaje;
+
+	//Datos de la Competencia
 	private Combobox cmbCompetencia;
 	private Datebox dtboxFechaIni;
 	private Datebox dtboxFechaFin;
 	private Textbox txtLugar;
+	//private ServicioCurso servicioCurso;
 	
-	private Combobox cmbCedulaNac;
-	private Intbox txtCedula;
+	//Datos del Representate
+	private Combobox cmbCedulaRep;
+	private Intbox txtCedulaRep;
 	private Button btnCatalogoRepresentante;
-	private Textbox txtNombreR;
-	private Textbox txtApellidoR;
-	private Textbox txtDireccionR;
-	
-	private Textbox txtNombreJ;
-	private Textbox txtApellidoJ;
-	private Combobox cmbCategoria;
-	private Textbox txtDireccionJ;
-	private Grid gridJugador;
-	
+	private Textbox txtNombreRep;
+	private Textbox txtApellidoRep;
+	private Textbox txtDireccionRep;
+
+	//Botones
 	private Button btnAgregar;
 	private Button btnQuitar;
 	private Button btnGuardar;
 	private Button btnCancelar;
 	private Button btnSalir;
-	
-	
-	
-	public void onClick$btnCatalogoRepresentante() {
-		new Util().crearVentana("Jugador/Vistas/buscarRepresentante.zul", null, null);
-	}	
 
-	public void onClick$btnAgregar() {
-		
-	}	
+	private AnnotateDataBinder binder;
+	private String rutasJug= Rutas.JUGADOR.getRutaVista();
+
 	
-	public void onClick$btnQuitar() {
+	@Override
+	public void doAfterCompose(Component comp) throws Exception {
+		super.doAfterCompose(comp);
+		comp.setVariable("controller", this, false); // Hacemos visible el
+														// modelo para el
+														// databinder
+	}
+
+	//Set y Get
 		
+	public Combobox getCmbCompetencia() {
+		return cmbCompetencia;
+	}
+
+	public void setCmbCompetencia(Combobox cmbCompetencia) {
+		this.cmbCompetencia = cmbCompetencia;
+	}
+
+	public Datebox getDtboxFechaIni() {
+		return dtboxFechaIni;
+	}
+
+	public void setDtboxFechaIni(Datebox dtboxFechaIni) {
+		this.dtboxFechaIni = dtboxFechaIni;
+	}
+
+	public Datebox getDtboxFechaFin() {
+		return dtboxFechaFin;
+	}
+
+	public void setDtboxFechaFin(Datebox dtboxFechaFin) {
+		this.dtboxFechaFin = dtboxFechaFin;
+	}
+
+	public Textbox getTxtLugar() {
+		return txtLugar;
+	}
+
+	public void setTxtLugar(Textbox txtLugar) {
+		this.txtLugar = txtLugar;
+	}
+
+	public Combobox getCmbCedulaRep() {
+		return cmbCedulaRep;
+	}
+
+	public void setCmbCedulaRep(Combobox cmbCedulaRep) {
+		this.cmbCedulaRep = cmbCedulaRep;
+	}
+
+	public Intbox getTxtCedulaRep() {
+		return txtCedulaRep;
+	}
+
+	public void setTxtCedulaRep(Intbox txtCedulaRep) {
+		this.txtCedulaRep = txtCedulaRep;
+	}
+
+	public Textbox getTxtNombreRep() {
+		return txtNombreRep;
+	}
+
+	public void setTxtNombreRep(Textbox txtNombreRep) {
+		this.txtNombreRep = txtNombreRep;
+	}
+
+	public Textbox getTxtApellidoRep() {
+		return txtApellidoRep;
+	}
+
+	public void setTxtApellidoRep(Textbox txtApellidoRep) {
+		this.txtApellidoRep = txtApellidoRep;
+	}
+
+	public Textbox getTxtDireccionRep() {
+		return txtDireccionRep;
+	}
+
+	public void setTxtDireccionRep(Textbox txtDireccionRep) {
+		this.txtDireccionRep = txtDireccionRep;
+	}
+
+	
+	//Metodos
+	
+	public void onCreate$win(ForwardEvent event){
+		 binder = (AnnotateDataBinder) event.getTarget().getVariable("binder", false);  
 	}
 	
+	public void onClick$btnCatalogoRepresentante() {
+		new Util().crearVentana(rutasJug+"buscarRepresentante.zul", null, null);
+	}
+
+	/*public List<Curso> getCursos() {
+		return servicioCurso.listar();
+	}*/
+
+	public void onClick$btnAgregar() {
+
+	}
+
+	public void onClick$btnQuitar() {
+
+	}
+
 	public void onClick$btnGuardar() {
 		
 	}
-	
+
 	public void onClick$btnCancelar() {
-		
+
 	}
-	
+
 	public void onClick$btnSalir() {
-		registrarHospedaje.detach();
+
+		winRegistrarHospedaje.detach();
 	}
+
 }
