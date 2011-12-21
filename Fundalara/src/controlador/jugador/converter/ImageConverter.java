d0ed9f74fbd63132dee1c146a87c007ec1584096
GermanLaguna <GermanLaguna@190.199.143.239>
2011-12-21 00:25:46 -0400

    Agregar paquetes de jugador

---
+++ Fundalara/src/controlador/jugador/converter/ImageConverter.java
@@ -0,0 +1,38 @@
+package controlador.jugador.converter;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
+import org.python.antlr.PythonParser.return_stmt_return;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zkplus.databind.TypeConverter;
+import org.zkoss.zul.Comboitem;
+import org.zkoss.zul.Image;
+import org.zkoss.zul.Radio;
+
+public class ImageConverter implements TypeConverter{
+
+	@Override
+	public Object coerceToBean(Object arg0, Component arg1) {
+	
+		 return null;
+
+	}
+
+	@Override
+	public Object coerceToUi(Object arg0, Component arg1) {
+		Image img ; 
+		if (arg0==null){
+			 img = new Image();
+			img.setSrc("../../Recursos/Imagenes/noFoto.jpg");
+			return img;//.getContent().getByteData();
+		
+		}else
+			return arg0;
+	}
+
+}
