2602163b928dbe37ea2472f009ee1fe98ed24402
Robert <usuario@190.39.246.144>
2011-12-21 13:07:42 -0430

    Agregando metodo daoEquipo equiposporcategoria

--- Fundalara/src/dao/general/DaoEquipo.java
+++ Fundalara/src/dao/general/DaoEquipo.java
@@ -1,7 +1,15 @@
 package dao.general;
 
+import java.util.List;
+
+import modelo.Equipo;
+
 import dao.generico.GenericDao;
 
 public class DaoEquipo extends GenericDao {
+	public List<Equipo> buscarEquiposPorCategoria(){
+		
+		return null;
+	}
 
 }
