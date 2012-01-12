e663bbfbbae670c69183c33fea5a991d6e2dcd64
Robert <usuario@190.39.246.144>
2012-01-12 00:06:58 -0430

    Agregando Mod Restriccion

--- Fundalara/src/controlador/jugador/restriccion/Restriccion.java
+++ Fundalara/src/controlador/jugador/restriccion/Restriccion.java
@@ -1,6 +1,6 @@
 package controlador.jugador.restriccion;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+
+import comun.Util;
 
 /**
  * Enumerado para mantener los valores constantes de los diferentes
@@ -10,20 +10,25 @@ import java.util.Calendar;
  * @author German L.
  * @version 0.1 03/01/2012
  */
+
 public enum Restriccion {
-	EMAIL("/.+@.+\\.[a-z]+/","Ingrese un correo electrónico válido, Ej: correo@servidor.com"),
-	HORAS_SEMANAL_SOCIAL("min 1 max 30","Ingrese una cantidad de horas válidas (01-30)"),
-	CEDULA("no negative,no empty","Ingrese una cédula válida"),
-	TELEFONO("/[0-9]{7}/","Ingrese un número válido, Ej: 2660011"),
-	TEXTO_SIMPLE("/^[a-zA-Z áéíóúAÉÍÓÚÑñ]+$/","Ingrese un texto válido"),
-	FECHA_NACIMIENTO("between "+ getFecha(17,'1') + " and "+ getFecha(3,'2'),"Ingrese una fecha válida");
+	EMAIL("/.+@.+\\.[a-z]+/",
+			"Ingrese un correo electrónico válido, Ej: correo@servidor.com"), HORAS_SEMANAL_SOCIAL(
+			"min 1 max 30", "Ingrese una cantidad de horas válidas (01-30)"), CEDULA(
+			"no negative,no empty", "Ingrese una cédula válida"), TELEFONO(
+			"/[0-9]{7}/", "Ingrese un número válido, Ej: 2660011"), TEXTO_SIMPLE(
+			"/^[a-zA-Z áéíóúAÉÍÓÚÑñ]+$/", "Ingrese un texto válido"), FECHA_NACIMIENTO(
+			"between " + Util.getFecha(Edad.EDAD_MAXIMA, '1') + " and "
+					+ Util.getFecha(Edad.EDAD_MINIMA, '2'),
+			"Ingrese una fecha válida");
 
 	private String constraint;
 	private String mensaje;
-	
+
 	private Restriccion(String constraint, String mensaje) {
 		this.constraint = constraint;
 		this.mensaje = mensaje;
+
 	}
 
 	public String getConstraint() {
@@ -37,10 +42,13 @@ public enum Restriccion {
 	public String getRestriccion() {
 		return constraint + " : " + mensaje;
 	}
-	
+
 	/**
-	 * Genera texto de restriccion tomando como base el constraint del enumerado añadiendo los pasados por parametro
-	 * @param restricciones arreglo de restricciones validas para objetos inputElement
+	 * Genera texto de restriccion tomando como base el constraint del enumerado
+	 * añadiendo los pasados por parametro
+	 * 
+	 * @param restricciones
+	 *            arreglo de restricciones validas para objetos inputElement
 	 * @return restriccion nueva con su mensaje de error
 	 */
 	public String asignarRestriccionExtra(String... restricciones) {
@@ -52,24 +60,4 @@ public enum Restriccion {
 		}
 		return restriccion + " : " + mensaje;
 	}
-	
-	/**
-	 * Obtiene la fecha correspondiente al limite inferior del rango de fechas de nacimiento.
-	 * @param Valor int (numerico) correspondiente a la edad minima o maxima.
-	 * @param Valor tipo char que define si la fecha solicitada es el limite inferior('1') o superior('2').
-	 * @return Cadena String con fecha en formato "yyyyMMdd".
-	 */
-	public static String getFecha (int valor, char opc) {
- 		Calendar hoy = Calendar.getInstance();
-		int anno = hoy.get(Calendar.YEAR) - valor;
-		Calendar fecha = Calendar.getInstance();
-		if (opc == '1'){
-			fecha.set(anno, Calendar.JANUARY, 1);
-		} else {
-			fecha.set(anno, Calendar.DECEMBER, 31);
-		}
-		SimpleDateFormat formato = new SimpleDateFormat("yyyyMMdd");
-		return formato.format(fecha.getTime());
- 	}
-
 }
