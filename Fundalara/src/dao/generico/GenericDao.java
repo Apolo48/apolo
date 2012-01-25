01a062a9ac84f40e463b9b836448ff4c43a318a8
RobertArrieche <robertArrieche@172.19.13.182>
2012-01-25 14:18:11 -0430

    Agregado cambio para solucionar problema de sesiones. Via Eduardo Ochoa

--- Fundalara/src/dao/generico/GenericDao.java
+++ Fundalara/src/dao/generico/GenericDao.java
@@ -2,7 +2,7 @@ package dao.generico;
 
 import java.util.List;
 
-import modelo.DatoBasico;
+import modelo.Divisa;
 
 import org.hibernate.Criteria;
 import org.hibernate.Session;
@@ -16,51 +16,99 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-@Service
+/**
+ * Clase para manejar las operaciones basicas mediante session factory
+ * @author Eduardo Ochoa
+ * @version 1.0 
+ * @email elos3000@gmail.com
+ */
 public class GenericDao {
 	
-	 Session session;
+	//static Session session;
     public void guardar(Object o) {		
-		Session session = getSession();
+    	Session session = SessionManager.getSession();
+		//Session session = getSession();
 		Transaction tx =  session.beginTransaction();
 		session.saveOrUpdate(o);
 		tx.commit();		
 	}
 
 	public void actualizar(Object o) {
-		Session session = getSession();
+		//Session session = getSession();
+		Session session = SessionManager.getSession();
 		Transaction tx =  session.beginTransaction();
 		session.saveOrUpdate(o);
 		tx.commit();
 	}
 	
 	public void eliminar(Object o) {
-		Session session = getSession();
+		//Session session = getSession();
+		Session session = SessionManager.getSession();
 		Transaction tx =  session.beginTransaction();
 		session.saveOrUpdate(o);
 	    tx.commit();
 	}
 	
+	public void eliminarFisico(Object o) {
+		//Session session = getSession();
+		Session session = SessionManager.getSession();
+		Transaction tx =  session.beginTransaction();
+		session.delete(o);
+	    tx.commit();
+	}
+	
 	public List listar(Class o) {
-		Session session = getSession();
+		//Session session = getSession();
+		Session session = SessionManager.getSession();
 		Transaction tx =  session.beginTransaction();
 		List lista = session.createCriteria(o).list();
 		return lista;
 	}
 	
-	public Session getSession(){
-		if (session == null){
-				return session = SessionManager.getSession();
-		}else{
-			  if(!session.isOpen())
-			      session = SessionManager.getSession();		    
-		      return session;	
-			}
-		}
+//	public Session getSession(){
+//	if (session == null){
+//			return session = SessionManager.getSession();
+//	}else{
+//		  if(!session.isOpen())
+//		      session = SessionManager.getSession();		    
+//	      return session;	
+//		}
+//	}
 	
+	public Session getSession(){
+		return SessionManager.getSession();
+	}
+	
+	public List listarActivos(Class d ){
+		//Permite buscar todas las divisas con estatus = 'A'
+		//Session session = getSession();
+		Session session = SessionManager.getSession();
+		Transaction tx = session.beginTransaction();
+		Criteria c = session.createCriteria(d);
+		List lista = c.add(Restrictions.eq("estatus",'A')).list();
+		return lista;
+	}
+	
+	public int generarCodigo(Class o) {
+		//Session session = getSession();
+		Session session = SessionManager.getSession();
+		Transaction tx =  session.beginTransaction();
+		return session.createCriteria(o).list().size()+1;
+	}
+	
+	public Object buscarUnCampo(Class o, String c, Object v) {
+		//ession session = getSession();
+		Session session = SessionManager.getSession();
+		Transaction tx =  session.beginTransaction();
+		Criteria cri = session.createCriteria(o);
+		cri.add(Restrictions.eq(c, v));
+		Object resultado = cri.list().get(0);
+	    return resultado;
+	}
 	
 	public Object buscarDosCampos(Class o, String c1, Object v1, String c2, Object v2) {
-		Session session = getSession();
+		//Session session = getSession();
+		Session session = SessionManager.getSession();
 		Transaction tx =  session.beginTransaction();
 		Criteria cri = session.createCriteria(o);
 		cri.add(Restrictions.eq(c1, v1));
@@ -68,6 +116,24 @@ public class GenericDao {
 		Object resultado = cri.list().get(0);
 	    return resultado;
 	}
-
+	public List listarUnCampo(Class o,String c, Object v) {
+		//Session session = getSession();
+		Session session = SessionManager.getSession();
+		Transaction tx =  session.beginTransaction();
+		Criteria cri = session.createCriteria(o);
+		cri.add(Restrictions.eq(c, v));
+		List lista = cri.list();
+		return lista;
+	}
+	
+	public List listarDosCampos(Class o,String c1, Object v1, String c2, Object v2) {
+		Session session = SessionManager.getSession();
+		Transaction tx =  session.beginTransaction();
+		Criteria cri = session.createCriteria(o);
+		cri.add(Restrictions.eq(c1, v1));
+		cri.add(Restrictions.eq(c2, v2));
+		List lista = cri.list();
+		return lista;
+	}
 }
 
