3f465f514b7972459b8667d95f36f858607b128e
Robert Arrieche <usuario@190.39.246.144>
2012-01-28 14:57:17 -0430

    Cambios en visor de documento

--- Fundalara/src/controlador/jugador/CntrlVisorDocumento.java
+++ Fundalara/src/controlador/jugador/CntrlVisorDocumento.java
@@ -5,18 +5,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URLConnection;
 
+import org.zkoss.image.AImage;
 import org.zkoss.util.media.AMedia;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.util.GenericForwardComposer;
 import org.zkoss.zul.Iframe;
+import org.zkoss.zul.Image;
 import org.zkoss.zul.Window;
 
 public class CntrlVisorDocumento extends GenericForwardComposer {
 	private Component formulario;
 	private Iframe iframeDocumento;
+	private Image imgImagen;
 	private Window winVisorDocumento;
 	byte[] archivo = null;
-
+	AMedia amedia = null;
 	@Override
 	public void doAfterCompose(Component comp) throws Exception {
 		super.doAfterCompose(comp);
@@ -37,17 +40,38 @@ public class CntrlVisorDocumento extends GenericForwardComposer {
 			}
 			String[] formato = new String[2];
 			if (mime == null) {
-				formato[0] = "pdf";
+				formato[0] = "";
 				formato[1] = "pdf";
 				mime = "application/pdf";
 			} else {
 				formato = mime.split("/");
 			}
-			AMedia amedia = new AMedia("archivo." + formato[1], formato[1],
+			 amedia = new AMedia("archivo." + formato[1], formato[1],
 					mime, mediais);
-			iframeDocumento.setContent(amedia);
-
+			if (formato[0].equals("image")){
+				iframeDocumento.setVisible(false);
+				AImage imagen=null;
+				try {
+					imagen = new AImage("imagen."+formato[1], mediais);
+				} catch (IOException e) {
+					e.printStackTrace();
+				} 
+				imgImagen.setContent(imagen);
+				imgImagen.setVisible(true);
+				winVisorDocumento.setHeight("300px");
+				winVisorDocumento.setWidth("200px");
+			}else{
+				iframeDocumento.setContent(amedia);	
+			}
 		}
 
+	}
+	public void onClick$btnSalir(){
+		onClose$winVisorDocumento();
+	}
+	public void onClose$winVisorDocumento( ) {
+		 amedia=null;
+		 winVisorDocumento.detach();
+	
 	}
 }
