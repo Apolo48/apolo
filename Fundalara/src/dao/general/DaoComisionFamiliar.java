c949a4068ddf4057450b9498540f8495842ccd22
Robert Arrieche <usuario@190.39.246.144>
2012-01-30 18:03:46 -0430

    Act de dao comision

--- Fundalara/src/dao/general/DaoComisionFamiliar.java
+++ Fundalara/src/dao/general/DaoComisionFamiliar.java
@@ -3,17 +3,34 @@ package dao.general;
 import java.util.List;
 
 import org.hibernate.Criteria;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.criterion.Restrictions;
 
 import modelo.ComisionFamiliar;
+import modelo.DatoBasico;
 import modelo.Familiar;
 
 import dao.generico.GenericDao;
 
+/**
+ * Clase DAO para acceso/manejo de las comisiones de un familiar
+ * 
+ * @author Robert A.
+ * @author German L.
+ * @version 0.1 30/01/2012
+ * 
+ */
 public class DaoComisionFamiliar extends GenericDao {
 
+	/**
+	 * Guarda/actualiza las comisiones de la lista de familiares , en caso de
+	 * enviar comision null, se interpretara como un familiar sin comisiones
+	 * 
+	 * @param comisionesFamiliares
+	 *            lista de comisiones de los familiares
+	 */
 	public void guardar(List<ComisionFamiliar> comisionesFamiliares) {
 
 		List<ComisionFamiliar> comisionesAlmacenadasPorFamiliar = null;
@@ -25,44 +42,46 @@ public class DaoComisionFamiliar extends GenericDao {
 					comisionFamiliar.getFamiliarJugador().getFamiliar()
 							.getCedulaRif()))) {
 				if (familiar != null) {
-					// condicion  de tamaaño
 					for (ComisionFamiliar comisionNoEnviadas : comisionesAlmacenadasPorFamiliar) {
 						comisionNoEnviadas.setEstatus('E');
-						super.actualizar(comisionNoEnviadas);
+						session.update(comisionNoEnviadas);
 					}
 				}
-				Criteria c = session.createCriteria(ComisionFamiliar.class)
-				.createCriteria("familiarJugador")
-				.add(Restrictions.eq("familiar", comisionFamiliar
-						.getFamiliarJugador().getFamiliar()))
-				.add(Restrictions.eq("estatus", 'A'));
+				Criteria c = session
+						.createCriteria(ComisionFamiliar.class)
+						.createCriteria("familiarJugador")
+						.add(Restrictions.eq("familiar", comisionFamiliar
+								.getFamiliarJugador().getFamiliar()))
+						.add(Restrictions.eq("estatus", 'A'));
 				comisionesAlmacenadasPorFamiliar = c.list();
-			} 
+			}
 
 			if (comisionFamiliar.getDatoBasico() != null) {
-				
-				System.out.println("Imprimiendo  la tira sql");
-				Criteria c2 = session.createCriteria(ComisionFamiliar.class)
-				.createCriteria("familiarJugador")
-				.add(Restrictions.eq("familiar", comisionFamiliar
-						.getFamiliarJugador().getFamiliar()))
-				.add(Restrictions.eq("estatus", 'A'))
-				.createCriteria("datoBasico")
-				.add(Restrictions.eq("codigoDatoBasico", comisionFamiliar
-						.getDatoBasico().getCodigoDatoBasico()));
-				ComisionFamiliar comisionAlmacenadaPorFamiliar = (ComisionFamiliar) c2
+				String cedula = comisionFamiliar.getFamiliarJugador()
+						.getFamiliar().getCedulaRif();
+				int codigoComision = comisionFamiliar.getDatoBasico()
+						.getCodigoDatoBasico();
+				Query query = session
+						.createSQLQuery(
+								"SELECT cf.codigo_comision_familiar, cf.codigo_familiar_jugador,  cf.codigo_comision, cf.estatus FROM comision_familiar cf, familiar_jugador fj WHERE  cf.codigo_familiar_jugador=fj.codigo_familiar_jugador  AND fj.cedula_familiar='"
+										+ cedula
+										+ "' AND fj.estatus='A' AND cf.codigo_comision="
+										+ codigoComision + "").addEntity(
+								ComisionFamiliar.class);
+				ComisionFamiliar comisionAlmacenadaPorFamiliar = (ComisionFamiliar) query
 						.uniqueResult();
-				System.out.println(" FIN Imprimiendo  la tira sql");
+
 				if (comisionAlmacenadaPorFamiliar == null) {
+
 					comisionFamiliar.setEstatus('A');
-					super.guardar(comisionFamiliar);
+					session.save(comisionFamiliar);
 				} else {
 					depurarLista(comisionesAlmacenadasPorFamiliar,
 							comisionAlmacenadaPorFamiliar);
 					if (comisionAlmacenadaPorFamiliar.getEstatus() == 'E') {
 						comisionAlmacenadaPorFamiliar.setEstatus('A');
 					}
-					super.actualizar(comisionAlmacenadaPorFamiliar);
+					session.update(comisionAlmacenadaPorFamiliar);
 				}
 			}
 			familiar = comisionFamiliar.getFamiliarJugador().getFamiliar();
@@ -71,13 +90,16 @@ public class DaoComisionFamiliar extends GenericDao {
 			comisionNoEnviadas.setEstatus('E');
 			session.update(comisionNoEnviadas);
 		}
-		//tx.commit();
+		tx.commit();
 	}
 
+	/**
+	 * Elimina las comisiones que ya han sido actualizadas de la lista auxiliar
+	 * @param lista
+	 * @param comisionRemover
+	 */
 	private void depurarLista(List<ComisionFamiliar> lista,
 			ComisionFamiliar comisionRemover) {
-		System.out.println("antes "+lista.size());
 		lista.remove(comisionRemover);
-		System.out.println("despues "+lista.size());
 	}
 }
