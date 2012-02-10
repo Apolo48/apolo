059b66950882cc9ba75564d3ea3d8903268d2ee5
RobertArrieche <robertArrieche@172.19.13.182>
2012-02-10 11:31:19 -0430

    Actualizando anuario

--- Fundalara/src/dao/general/DaoAnuario.java
+++ Fundalara/src/dao/general/DaoAnuario.java
@@ -1,7 +1,33 @@
 package dao.general;
 
+import java.util.Date;
+
+import modelo.DatoBasico;
+import modelo.LapsoDeportivo;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
+
 import dao.generico.GenericDao;
 
 public class DaoAnuario extends GenericDao {
+	
+	public void guardar(){
+		DatoBasico lapso= new DatoBasico();
+		Date fecha = new Date();
+		//Buscar al lapso FALTA
+		Session session = getSession();
+		Transaction tx = session.beginTransaction();
+		
+		Criteria c = session.createCriteria(LapsoDeportivo.class)
+				.add(Restrictions.eq("tipoLapso", lapso))
+				.add(Restrictions.le("fechaInicio", fecha))
+				.add(Restrictions.ge("fechaFin",fecha ))
+				.add(Restrictions.eq("estatus", 'A'));
+
+		tx.commit();
+	}
 
 }
